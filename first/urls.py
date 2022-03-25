fa7075e4f0af2219fe3a1cd513335daff165f4a6
VirangParekh <44228173+VirangParekh@users.noreply.github.com>
2022-03-26 01:24:30 +0530

    Project side setup completed

--- first/urls.py
+++ first/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('expense/',include("expense.urls")),
+    # path('expense/',include("expense.urls")),
     path('register/',views.register, name='register'),
     path('login/',views.login, name='login'),
     path('logout/',views.logout, name='logout'),
